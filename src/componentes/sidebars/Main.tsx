296d0cfcfb6dcc8c19eec191c6c1c4a84486c3e1
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-22 16:57:51 +0200

    Icon button for non desktop

--- src/componentes/sidebars/Main.tsx
+++ src/componentes/sidebars/Main.tsx
@@ -4,6 +4,7 @@ import { IoNotificationsCircleOutline } from 'solid-icons/io';
 import { RiMapCompassDiscoverLine } from 'solid-icons/ri';
 import { AiOutlineHome } from 'solid-icons/ai';
 import { FiMoreHorizontal } from 'solid-icons/fi';
+import { HiOutlinePencilSquare } from 'solid-icons/hi';
 
 const MainSidebar: Component = () => {
   return (
@@ -11,7 +12,7 @@ const MainSidebar: Component = () => {
       <div class='xl:w-80 w-20 flex-it'>
         <div class='h-full fixed flex-it top-0'>
           <div class='flex-it h-full xl:w-80 w-20 overflow-y-auto px-3 justify-between'>
-            <div class='flex-it items-start'>
+            <div class='flex-it items-center xl:items-start'>
               <div class='p-3 pt-4 xl:pb-3 pb-0 xl:text-2xl text-sm font-bold transition duration-200 hover:opacity-80'>
                 <a href='#'>
                   <h1>Glider</h1>
@@ -73,11 +74,14 @@ const MainSidebar: Component = () => {
               </div>
               {/* GLIDER SEND-MESSAGE BUTTON */}
               <div class='my-1 flex-it w-10/12 cursor-pointer'>
-                <div class='bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition'>
+                <div class='bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition hidden xl:flex'>
                   <div class='flex-it flex-row text-xl font-bold text-white items-start justify-center truncate duration-200'>
                     <div>Glide It</div>
                   </div>
                 </div>
+                <button class='bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition h-8 w-8 flex justify-center items-center xl:hidden'>
+                  <HiOutlinePencilSquare class='h-5 w-5' />
+                </button>
               </div>
             </div>
             {/* PROFILE MENU */}
