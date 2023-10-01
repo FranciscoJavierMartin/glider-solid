d999bc99bf67af9bf69e543b71544ee84f9c50d3
Francisco <javiermartincuenca@gmail.com>
2023-10-01 11:46:59 +0200

    Update user interface

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,12 @@
+import { DocumentReference } from 'firebase/firestore';
+
 export interface User {
+  uid: string;
   nickName: string;
+  fullName: string;
   avatar: string;
+  followers: DocumentReference;
+  following: DocumentReference;
+  followersCount: number;
+  followingCount: number;
 }
