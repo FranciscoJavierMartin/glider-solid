c3d7a0e645eb83e5330ab8625e402cad0661aa3c
Francisco <javiermartincuenca@gmail.com>
2023-10-04 23:33:14 +0200

    Auto hide snackbar

--- src/components/snackbar/index.tsx
+++ src/components/snackbar/index.tsx
@@ -1,12 +1,42 @@
-import { Component } from 'solid-js';
+import {
+  Component,
+  createEffect,
+  createSignal,
+  mergeProps,
+  onCleanup,
+  onMount,
+} from 'solid-js';
 import { IoCloseCircle } from 'solid-icons/io';
 import { SnackbarMessage } from '../../context/ui';
 
 type SnackbarProps = SnackbarMessage & {
   onClose: () => void;
+  autoHideDuration?: number;
 };
 
-export const Snackbar: Component<SnackbarProps> = (props) => {
+export const Snackbar: Component<SnackbarProps> = (initialProps) => {
+  const props = mergeProps({ autoHideDuration: 2000 }, initialProps);
+  const [duration, setDuration] = createSignal<number>(props.autoHideDuration);
+
+  let timerId: number;
+
+  onMount(() => {
+    timerId = window.setInterval(() => {
+      setDuration((prev) => prev - 50);
+    }, 50);
+  });
+
+  createEffect(() => {
+    if (duration() <= 0) {
+      window.clearInterval(timerId);
+      props.onClose();
+    }
+  });
+
+  onCleanup(() => {
+    window.clearInterval(timerId);
+  });
+
   return (
     <div
       class='min-w-68 text-white flex-it font-bold rounded-md md:max-w-xs w-full text-sm shadow-md'
