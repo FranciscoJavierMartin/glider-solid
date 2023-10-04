a0d58a174c870daff8e786af22af911fac35e405
Francisco <javiermartincuenca@gmail.com>
2023-10-04 23:49:10 +0200

    Add snackbar to authenticate

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,18 +1,21 @@
 import { createSignal } from 'solid-js';
+import { FirebaseError } from 'firebase/app';
 import { AuthType, authenticate } from '../api/auth';
 import { AuthForm } from '../types/form';
-import { FirebaseError } from 'firebase/app';
+import { useUIDispatch } from '../context/ui';
 
 const useAuth = (authType: AuthType) => {
   const [isLoading, setIsLoading] = createSignal<boolean>(false);
+  const { addSnackbar } = useUIDispatch();
 
   const authUser = async (form: AuthForm) => {
     setIsLoading(true);
     try {
       await authenticate(form, authType);
+      addSnackbar({ message: 'Welcome to Glider', type: 'success' });
     } catch (error) {
       const message = (error as FirebaseError).message;
-      console.log(message);
+      addSnackbar({ message, type: 'error' });
     } finally {
       setIsLoading(false);
     }
