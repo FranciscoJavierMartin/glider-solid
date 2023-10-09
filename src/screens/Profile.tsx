a0b51421341f5af5e29e07b84536eae93e45cb6d
Francisco <javiermartincuenca@gmail.com>
2023-10-09 21:31:22 +0200

    Add loader

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,12 @@
-import { Component, For } from 'solid-js';
+import { Component, For, Show } from 'solid-js';
 import MainLayout from '../layouts/MainLayout';
 import { useAuthState } from '../context/auth';
 import useUsers from '../hooks/useUsers';
+import { CenteredDataLoader } from '../components/utils/DataLoader';
 
 const ProfileScreen: Component = () => {
   const authState = useAuthState()!;
-  const { users } = useUsers();
+  const { users, isLoading } = useUsers();
 
   return (
     <MainLayout pageTitle='Profile'>
@@ -27,35 +28,37 @@ const ProfileScreen: Component = () => {
             </div>
           </div>
         </div>
-        <For each={users()}>
-          {(user) => (
-            <div class='flex-it p-4'>
-              <div class='flex-it flex-row'>
-                <div class='flex-it mr-4'>
-                  <div class='w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80'>
-                    <img class='rounded-full' src={user.avatar} />
-                  </div>
-                </div>
-                <article class='flex-it flex-grow flex-shrink'>
-                  <div class='flex-it justify-center flex-grow mb-1'>
-                    <div class='flex-it justify-between flex-row w-full'>
-                      <div class='flex-it justify-center'>
-                        <span class='font-bold'>{user.nickName}</span>
-                      </div>
-                      <div class='flex-it w-32 mt-3 cursor-pointer'>
-                        <button class='disabled:cursor-not-allowed disabled:bg-gray-400 bg-blue-400 hover:bg-blue-500 font-bold py-2 px-4 rounded-full flex-it transition duration-200'>
-                          <div class='flex-it flex-row text-sm font-bold text-white items-start justify-center'>
-                            <span>Follow</span>
-                          </div>
-                        </button>
-                      </div>
+        <Show when={!isLoading()} fallback={<CenteredDataLoader />}>
+          <For each={users()}>
+            {(user) => (
+              <div class='flex-it p-4'>
+                <div class='flex-it flex-row'>
+                  <div class='flex-it mr-4'>
+                    <div class='w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80'>
+                      <img class='rounded-full' src={user.avatar} />
                     </div>
                   </div>
-                </article>
+                  <article class='flex-it flex-grow flex-shrink'>
+                    <div class='flex-it justify-center flex-grow mb-1'>
+                      <div class='flex-it justify-between flex-row w-full'>
+                        <div class='flex-it justify-center'>
+                          <span class='font-bold'>{user.nickName}</span>
+                        </div>
+                        <div class='flex-it w-32 mt-3 cursor-pointer'>
+                          <button class='disabled:cursor-not-allowed disabled:bg-gray-400 bg-blue-400 hover:bg-blue-500 font-bold py-2 px-4 rounded-full flex-it transition duration-200'>
+                            <div class='flex-it flex-row text-sm font-bold text-white items-start justify-center'>
+                              <span>Follow</span>
+                            </div>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </article>
+                </div>
               </div>
-            </div>
-          )}
-        </For>
+            )}
+          </For>
+        </Show>
       </div>
     </MainLayout>
   );
